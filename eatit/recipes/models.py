b409952c484a9f61861a02acf08c09687bf91813
QuietSup <feeling_of_infinity@outlook.com>
2021-11-26 19:31:01 +0200

    added classes to models.py

--- eatit/recipes/models.py
+++ eatit/recipes/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.db.models import UniqueConstraint
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 class Recipe(models.Model):
     name = models.CharField('Recipe name', max_length=50)
     photo = models.ImageField('Photo', upload_to='photos')
-    ingredients = models.TextField('Ingredients', null=True)
+    content = models.TextField('Content', blank=True)
     time_created = models.DateTimeField('Creation time', auto_now_add=True)
     time_updated = models.DateTimeField('Update time', auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     cat = models.ManyToManyField('Category', verbose_name='Categories')
     ingr = models.ManyToManyField('Ingredient', verbose_name='Ingredients')
 
@@ -30,10 +33,22 @@ class Category(models.Model):
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=20)
-    calories = models.IntegerField('Calories number', max_length=5)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('ingredient', kwargs={'ingr_id': self.pk})
+
+
+class Like(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    post_id = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='likes')
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['user_id', 'post_id'], name="unique_blog_likes")
+        ]
+
+    def __str__(self):
+        return f'{self.user_id.username} --> {self.post_id.name}'
--- eatit/recipes/urls.py
+++ eatit/recipes/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from .views import *
 
-urlpatterns = {
+urlpatterns = [
     path('', index),
     path('login/', login, name='login'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('category/<int:cat_id>/', show_category, name='category'),
-}
+]
